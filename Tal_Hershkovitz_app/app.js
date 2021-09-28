2dc243ca1f5786144e6c257391af1f5a06ff1309
talHershkovitz <talher@post.bgu.ac.il>
2021-09-28 19:50:26 +0300

    hw7

--- Tal_Hershkovitz_app/app.js
+++ Tal_Hershkovitz_app/app.js
@@ -1,41 +1,46 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+//import modules
+const express = require('express');
+const path = require('path');
+// init the app
+const app = express();
+// static files
+// app.use('/static', express.static(path.join(__dirname, 'public')))
+// home route
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const publicDirectory = path.join(__dirname,'./public');
+app.use(express.static(publicDirectory));
 
-var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, '/views/html/CV.html'));
+});
+app.get('/details', function (req, res) {
+  res.sendFile(path.join(__dirname, '/views/html/details.html'));
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+app.get('/education', function (req, res) {
+  res.sendFile(path.join(__dirname, '/views/html/education.html'));
 });
 
-module.exports = app;
+app.get('/experience', function (req, res) {
+  res.sendFile(path.join(__dirname, '/views/html/experience.html'));
+});
+
+app.get('/MilitaryService', function (req, res) {
+  res.sendFile(path.join(__dirname, '/views/html/MilitaryService.html'));
+});
+
+app.get('/contactUs', function (req, res) {
+  res.sendFile(path.join(__dirname, '/views/html/contactUs.html'));
+});
+
+
+
+
+
+
+// set server to listen at port
+app.listen(3000, () => {
+  console.log('Server is renning at port 3000...')
+});
